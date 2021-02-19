cfc15131bac7b60961334bd271f67253753906a6
danver <podzyuban@gmail.com>
2021-02-19 09:39:37 +0300

    f

--- otus/lab_K_5/helmdeploy.bat
+++ otus/lab_K_5/helmdeploy.bat
@@ -1,8 +1,9 @@
+helm install kafka bitnami/kafka -f ./kafka/kafka-config.yaml --set rbac.create=true
 helm install authservice .\pointservice-chart\
 helm install orderservice .\pointservice-orderservice\
+helm install kc .\kafka-connect
 helm install billingservice .\pointservice-billingservice\
 helm install notificationsservice .\pointservice-notificationservice\
 helm install api-gateway .\pointservice-apigateway\
-helm install kafka bitnami/kafka -f ./kafka/kafka-config.yaml --set rbac.create=true
-helm install kc .\kafka-connect
+
 
